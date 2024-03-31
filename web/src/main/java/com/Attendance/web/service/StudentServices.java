a11a6d0a12d436401befc95d548b7a4f09d63c0d
Pashidu Malshan <88223496+Phantom-LK@users.noreply.github.com>
2024-03-31 15:49:21 +0530

    Delete web directory

--- web/src/main/java/com/Attendance/web/service/StudentServices.java
+++
@@ -1,9 +0,0 @@
-package com.Attendance.web.service;
-
-import com.Attendance.web.dto.StudentDto;
-
-import java.util.List;
-
-public interface StudentServices {
-    List<StudentDto> findAllStudents();
-}
